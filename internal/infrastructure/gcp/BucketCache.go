8406faf22d60693d096e715e967f1ab60851defc
radu helstern <radu.helstern@gmail.com>
2020-03-04 20:00:15 +0000

    add synchronisation to the cache of website configurations

--- internal/infrastructure/gcp/BucketCache.go
+++ internal/infrastructure/gcp/BucketCache.go
@@ -1,6 +1,9 @@
 package gcp
 
-import "cloud.google.com/go/storage"
+import (
+	"cloud.google.com/go/storage"
+	"sync"
+)
 
 type BucketCacheItem struct {
 	website *storage.BucketWebsite
@@ -8,30 +11,34 @@ type BucketCacheItem struct {
 
 type BucketCache struct {
 	items map[string]BucketCacheItem
+	lock  sync.RWMutex
 }
 
 func (this *BucketCache) Get(key string) (*storage.BucketWebsite, bool) {
+
+	this.lock.RLock()
 	existingItem, exists := this.items[key]
+	this.lock.RUnlock()
+
 	if exists {
 		return existingItem.website, exists
 	}
-
 	return nil, exists
 }
 
-func (this *BucketCache) Put(key string, item *storage.BucketWebsite) *storage.BucketWebsite {
-	existingItem, exists := this.Get(key)
+func (this *BucketCache) Put(key string, item *storage.BucketWebsite) {
 	cacheItem := BucketCacheItem{
 		website: item,
 	}
-	this.items[key] = cacheItem
 
-	if exists {
-		return existingItem
-	}
-	return nil
+	this.lock.Lock()
+	this.items[key] = cacheItem
+	this.lock.Unlock()
 }
 
 func NewBucketCache() BucketCache {
-	return BucketCache{items: make(map[string]BucketCacheItem)}
+	return BucketCache{
+		items: make(map[string]BucketCacheItem),
+		lock:  sync.RWMutex{},
+	}
 }
